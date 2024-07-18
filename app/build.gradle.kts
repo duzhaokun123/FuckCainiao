daab6048d0ebac98d45c0d7dceaa98ceaf548a11
o0kam1 <duzhaokun2@outlook.com>
2024-07-18 20:54:39 +0800

    release: 0.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "io.github.duzhaokun123.fuckcainiao"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
+        versionCode = 6
         versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
